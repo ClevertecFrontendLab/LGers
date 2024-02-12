bb3875bfcf12bde0932aecc3f9683948c5e9f814
LGers <leonid.mychko2k21@gmail.com>
2024-02-12 17:44:24 +0300

    feat: add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,28 @@
-import styles from './Header.module.css';
+import s from './Header.module.scss';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
-
     return (
-        <header className={styles.header}>
-            <p className={styles.header}>header</p>
+        <header className={s.header}>
+            <div className={s.breadcrumbs}>
+                <Link to='/'>Главная</Link>
+            </div>
+            <div className={s.header__content}>
+                <h1 className={s.header__h1}>
+                    <span>
+                        Приветствуем тебя в CleverFit — приложении,
+                    </span>
+                    <span>
+                        которое поможет тебе добиться своей мечты!
+                    </span>
+                </h1>
+                <div className={s.header__settings}>
+                    <button  className={s.button}>
+                        <img className={s.button__icon}src="../../../public/assets/svg/settings.svg" alt="Settings" />
+                        <span className={s.button__text}>Настройки</span>
+                    </button>
+                </div>
+            </div>
         </header>
     );
 };
--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -2,21 +2,23 @@ import { Wrapper } from "@components/Wrapper";
 import { Sidebar } from "@components/Sidebar";
 import { Header } from "@components/Header";
 import { Footer } from "@components/Footer";
-import styles from './mainPage.module.css';
+import s from './mainPage.module.scss';
 
 export const MainPage = () => {
 
     return (
         <Wrapper>
-            <Sidebar/>
-            <div className={styles.pageContainer}>
-                <Header/>
-                <main className={styles.main}>
-                    <div>
-                        main
-                    </div>
-                </main>
-                <Footer/>
+            <div className={s.main__page}>
+                <Sidebar/>
+                <div className={s.pageContainer}>
+                    <Header/>
+                    <main className={s.main}>
+                        <div>
+                            main
+                        </div>
+                    </main>
+                    <Footer/>
+                </div>
             </div>
         </Wrapper>
     );
